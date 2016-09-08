f6ec203d3de2e74a54f9e18d7cafeecf931d716f
Cody Ogden <cody@codyogden.com>
2016-09-08 16:51:35 -0500

    Removed harrypotter in favor of app

--- public/scripts/client-script.js
+++ public/scripts/client-script.js
@@ -1,1 +1,40 @@
-console.log( "client-script is sourced" );
+var roundCount = 1;
+
+var startGame = function() {
+
+};
+
+var totalRounds = function() {
+	roundCount++;
+	$( '.total-rounds' ).html( roundCount );
+	return console.log( "Current Round:", roundCount );
+};
+
+var abortGame = function() {
+	// Someone clicks abort button
+	var abortCheck = confirm("Are you sure you'd like to abort?");
+
+	if( !abortCheck ) return false;
+
+
+
+};
+
+var endGame = function() {
+	// Someone wins
+};
+
+var submitGuess = function() {
+
+};
+
+var restartGame = function() {
+
+};
+
+$(document).ready(function() {
+	$( '.btn-start' ).on( 'click', startGame );
+	$( '.btn-abort' ).on( 'click', abortGame );
+	$( '.btn-restart' ).on( 'click', restartGame );
+	$( '.btn-guess' ).on( 'click', submitGuess );
+});
